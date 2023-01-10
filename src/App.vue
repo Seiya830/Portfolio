30da4f7167b5df7fa6b2452162eca23d47b1d331
Seiya830 <seiya@MacBook-Pro.local>
2023-01-10 16:14:36 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <h1>Portfolio</h1>
+  </header>
+  <main></main>
+  <RouterView />
+</template>
+
+<style></style>
