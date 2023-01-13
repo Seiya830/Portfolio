8feaa2b62f76c7b6d44cf1f4c9717827b7a77a53
Seiya830 <seiya@MacBook-Pro.local>
2023-01-13 23:41:25 +0900

    worksページ追加

---
+++ src/views/WorksView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="works">
+    <h1>ここはworksページです</h1>
+    <router-link to="/views/HomeView.vue">Homeへ</router-link>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
