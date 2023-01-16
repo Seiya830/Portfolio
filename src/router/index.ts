165b018334cdf694fb4b1284b71d6c269339e0e4
Seiya830 <seiya@MacBook-Pro.local>
2023-01-16 11:47:23 +0900

    contactページ追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
-import AboutViewVue from '@/views/AboutView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import AboutView from '../views/AboutView.vue'
 import HomeView from '../views/HomeView.vue'
 import WorksView from '../views/WorksView.vue'
+import ContactView from '../views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,12 +18,17 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: AboutViewVue
+      component: AboutView
     },
     {
       path: '/works',
       name: 'works',
       component: WorksView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
     }
   ]
 })
