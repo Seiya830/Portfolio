f3403a5d8c0c467e2a601e9165578d8ff7bd34c1
Seiya830 <seiya@MacBook-Pro.local>
2023-01-18 10:56:58 +0900

    ヘッダーにnavタグ追加

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,24 @@
 <template>
   <header>
-    <h1>Portfolio</h1>
+    <h1>Home</h1>
+    <nav>
+      <li>
+        <router-link to="/works">Works</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/contact">Contact</router-link>
+      </li>
+    </nav>
   </header>
 </template>
+
+<style lang="scss" scoped>
+li {
+  list-style: none;
+  text-align: center;
+  margin-bottom: 45px;
+}
+</style>
