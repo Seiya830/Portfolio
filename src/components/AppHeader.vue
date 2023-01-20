1765121322ad7ab2c18387518b825c830918174b
Seiya830 <seiya@MacBook-Pro.local>
2023-01-20 22:48:34 +0900

    ハンバーガーメニューのアニメーション実装

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,7 +11,11 @@ export default defineComponent({
 
 <template>
   <div>
-    <button @click="open = !open">Toggle menu</button>
+    <button @click="open = !open" :class="{ open: open }" class="hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
     <nav v-if="open">
       <ul>
         <li>
@@ -33,5 +37,56 @@ li {
   list-style: none;
   text-align: center;
   margin-bottom: 30px;
+}
+
+.hamburger {
+  position: fixed;
+  top: 0;
+  right: 0;
+  display: inline-block;
+  width: 75px;
+  height: 75px;
+  background-color: gray;
+  vertical-align: top;
+  border: none;
+  z-index: 999;
+  cursor: pointer;
+
+  span {
+    position: absolute;
+    top: 20px;
+    left: 18px;
+    display: inline-block;
+    width: 35px;
+    height: 2px;
+    background-color: black;
+    transition: ease 0.4s;
+  }
+
+  :nth-child(1) {
+    margin-top: 5px;
+  }
+  :nth-child(2) {
+    margin-top: 15px;
+  }
+  :nth-child(3) {
+    margin-top: 25px;
+  }
+
+  &.open {
+    span {
+      &:nth-child(1) {
+        transform: rotate(45deg);
+        margin-top: 15px;
+      }
+      &:nth-child(2) {
+        opacity: 0;
+      }
+      &:nth-child(3) {
+        transform: rotate(-45deg);
+        margin-top: 15px;
+      }
+    }
+  }
 }
 </style>
