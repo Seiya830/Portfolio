feaf32328559bd10098b9841e266424f5a394f9a
Seiya830 <seiya@MacBook-Pro.local>
2023-01-31 17:39:43 +0900

    top-title追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
-  <main>
-    <div></div>
-    <p>ここはHomeページです</p>
-  </main>
+  <div class="top-title">
+    <p>Seiya's Portfolio</p>
+  </div>
 </template>
+
+<style lang="scss">
+.top-title {
+  height: 500px;
+
+  p {
+    line-height: 500px;
+    text-align: center;
+    font-size: 38px;
+  }
+}
+</style>
