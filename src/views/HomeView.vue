dca43daab2bae8cb087a7971725cf01a663a1b3f
Seiya830 <seiya@MacBook-Pro.local>
2023-02-03 23:46:44 +0900

    top-titleを上下左右中央揃えにして追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="top-title">
-    <p>Seiya's Portfolio</p>
+    <div class="home-text">
+      <h2>SEIYA OMORI</h2>
+      <h3>Portfolio</h3>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .top-title {
   height: 500px;
+  font-size: 38px;
+  position: relative;
 
-  p {
-    line-height: 500px;
+  .home-text {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: 50%;
+    height: 250px;
     text-align: center;
-    font-size: 38px;
   }
 }
 </style>
