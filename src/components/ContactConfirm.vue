294093bcdb2c379449df5464ad7cac84ca1f6795
Seiya830 <seiya@MacBook-Pro.local>
2023-03-06 11:30:01 +0900

    問い合わせフォームの確認画面追加

---
+++ src/components/ContactConfirm.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    name: String,
+    email: String,
+    message: String,
+  },
+
+  methods: {
+    submitForm() {
+      // フォームの送信処理を実装する
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <p>以下の内容でよろしいですか？</p>
+
+    <ul>
+      <li>名前: {{ name }}</li>
+      <li>メールアドレス: {{ email }}</li>
+      <li>メッセージ: {{ message }}</li>
+    </ul>
+
+    <button @click="submitForm">送信</button>
+  </div>
+</template>
