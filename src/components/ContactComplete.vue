7cd2e033f990c42b693b09f8effd267c6ce2ae17
Seiya830 <seiya@MacBook-Pro.local>
2023-03-08 10:26:08 +0900

    完了画面のhomeへ戻るリンクを追加

--- src/components/ContactComplete.vue
+++ src/components/ContactComplete.vue
@@ -5,7 +5,37 @@ export default defineComponent({});
 </script>
 
 <template>
-  <div>
+  <div class="complete">
     <p>お問い合わせありがとうございました。</p>
+    <router-link to="/">Homeに戻る</router-link>
   </div>
 </template>
+
+<style lang="scss">
+.complete {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f9f9f9;
+
+  p {
+    font-size: 24px;
+    margin: 20px;
+    text-align: center;
+  }
+
+  a {
+    font-size: 16px;
+    color: #007bff;
+    text-decoration: underline;
+    margin-top: 20px;
+    cursor: pointer;
+
+    &:hover {
+      color: #0056b3;
+    }
+  }
+}
+</style>
