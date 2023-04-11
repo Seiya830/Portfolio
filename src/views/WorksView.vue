04b99f451f876dbee612be2706b0ab1855188ff6
Seiya830 <seiya@MacBook-Pro.local>
2023-04-11 11:15:54 +0900

    過去の成果物追加

--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -2,7 +2,16 @@
 
 <template>
   <div class="works">
-    <h1>ここはworksページです</h1>
+    <a href="https://seiya830.github.io/sauna-quiz/" target="_blank"
+      >サウナクイズ</a
+    >
+    <a href="https://seiya830.github.io/english-school/" target="_blank"
+      >架空英会話教室サイト</a
+    >
+    <a href="https://seiya830.github.io/vue_janken/" target="_blank"
+      >Vue3じゃんけんゲーム</a
+    >
+    <a href="https://seiya830.github.io/M-Test/" target="_blank">Mテスト</a>
     <router-link to="/">Homeへ</router-link>
   </div>
 </template>
