dade7fc805e982aed12ce313c588342fb8d4b880
Seiya830 <seiya@MacBook-Pro.local>
2023-04-11 11:41:18 +0900

    説明文、スタイルの追加

--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -1,19 +1,115 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="works">
-    <a href="https://seiya830.github.io/sauna-quiz/" target="_blank"
-      >サウナクイズ</a
-    >
-    <a href="https://seiya830.github.io/english-school/" target="_blank"
-      >架空英会話教室サイト</a
-    >
-    <a href="https://seiya830.github.io/vue_janken/" target="_blank"
-      >Vue3じゃんけんゲーム</a
-    >
-    <a href="https://seiya830.github.io/M-Test/" target="_blank">Mテスト</a>
-    <router-link to="/">Homeへ</router-link>
+    <div class="works__item">
+      <a href="https://seiya830.github.io/sauna-quiz/" target="_blank">
+        <img
+          class="works__thumbnail"
+          src="https://placehold.jp/300x300.png"
+          alt="サウナクイズ"
+        />
+        <div class="works__description">
+          <h3 class="works__title">サウナクイズ</h3>
+          <p class="works__text">
+            4択クイズ形式でサウナに関する問題を出題するWebアプリケーションです。
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="works__item">
+      <a href="https://seiya830.github.io/english-school/" target="_blank">
+        <img
+          class="works__thumbnail"
+          src="https://placehold.jp/300x300.png"
+          alt="架空英会話教室サイト"
+        />
+        <div class="works__description">
+          <h3 class="works__title">架空英会話教室サイト</h3>
+          <p class="works__text">
+            架空の英会話教室のウェブサイトを模した作品です。
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="works__item">
+      <a href="https://seiya830.github.io/vue_janken/" target="_blank">
+        <img
+          class="works__thumbnail"
+          src="https://placehold.jp/300x300.png"
+          alt="Vue3じゃんけんゲーム"
+        />
+        <div class="works__description">
+          <h3 class="works__title">Vue3じゃんけんゲーム</h3>
+          <p class="works__text">
+            Vue3で作成したじゃんけんゲームです。人間とコンピュータの対戦ができます。
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="works__item">
+      <a href="https://seiya830.github.io/M-Test/" target="_blank">
+        <img
+          class="works__thumbnail"
+          src="https://placehold.jp/300x300.png"
+          alt="Mテスト"
+        />
+        <div class="works__description">
+          <h3 class="works__title">Mテスト</h3>
+          <p class="works__text">
+            鍼灸師の治療法の1つとして使われているものを診断テスト形式で作成しました。
+          </p>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.works {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-gap: 2rem;
+  margin-top: 2rem;
+}
+
+.works__item {
+  position: relative;
+  overflow: hidden;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
+.works__thumbnail {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+
+.works__description {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 1rem;
+  box-sizing: border-box;
+  color: #fff;
+  transition: all 0.3s ease;
+  transform: translateY(100%);
+}
+
+.works__item:hover .works__description {
+  transform: translateY(0);
+}
+
+.works__title {
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+}
+
+.works__text {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+</style>
