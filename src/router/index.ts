e2892bb4fc6af915e30d810e8c47f7c54784d050
Seiya830 <seiya@MacBook-Pro.local>
2023-09-11 11:19:24 +0900

    パス変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import ContactView from '../views/ContactView.vue'
 import ContactComplete from '../components/ContactComplete.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
@@ -29,7 +29,7 @@ const router = createRouter({
       component: ContactView
     },
     {
-      path: '/components/ContactComplete',
+      path: '/ContactComplete',
       name: 'contact-form-complete',
       component: ContactComplete,
     },
